b3d5b0876974bc13f54ba025a07605709af83c2e
lebbis <44391814+lebbis@users.noreply.github.com>
2021-02-10 18:50:40 +0200

    commit

---
+++ pages/api/hello.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log(req)
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ name: 'POST' }))
+} else {
+  res.end(JSON.stringify({ name: 'GET' }))
+}
+}
